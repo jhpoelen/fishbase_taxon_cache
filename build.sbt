5be17b7f8d8cc129708ec311f581a3e09c4e1c07
jhpoelen <jhpoelen@xs4all.nl>
2017-05-05 20:46:59 -0700

    fix issue #1

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ import sbt.Keys._
 
 lazy val commonSettings = Seq(
   organization := "com.github.jhpoelen",
-  version := "0.1.1",
+  version := "0.2.0",
   scalaVersion := "2.11.8"
 )
 
--- src/main/scala/com/github/jhpoelen/FishbaseTaxonCache.scala
+++ src/main/scala/com/github/jhpoelen/FishbaseTaxonCache.scala
@@ -47,7 +47,7 @@ trait DB {
 }
 
 object FishBase extends DB {
-  override def tables = Tables(species = "species", taxa = "taxa", stocks = "stocks")
+  override def tables = Tables(species = "species_fishbase", taxa = "taxa_fishbase", stocks = "stocks_fishbase")
 
 
   override def host = "fishbase.org"
@@ -56,7 +56,7 @@ object FishBase extends DB {
 }
 
 object SeaLifeBase extends DB {
-  override def tables = Tables(species = "sealifebase_species", taxa = "sealifebase_taxa", stocks = "sealifebase_stocks")
+  override def tables = Tables(species = "species_sealifebase", taxa = "taxa_sealifebase", stocks = "stocks_sealifebase")
 
   override def host = "sealifebase.org"
 
@@ -66,7 +66,7 @@ object SeaLifeBase extends DB {
 
 object FishbaseTaxonCache extends App {
 
-  val baseUrl = "https://github.com/jhpoelen/fishbase_archiver/releases/download/v0.1.0/"
+  val baseUrl = "https://github.com/jhpoelen/fishbase_archiver/releases/download/v0.2.1/"
   val ext = ".tsv.gz"
 
   // read stocks, extract species ids
@@ -84,81 +84,83 @@ object FishbaseTaxonCache extends App {
     s"$baseUrl$tableName$ext"
   }
 
-  val sealifebase = Tables(species = "sealifebase_species", taxa = "sealifebase_taxa", stocks = "sealifebase_stocks")
+  val dbs = List(SeaLifeBase)
 
-  val dbs = List(FishBase, SeaLifeBase)
+  def taxonMapWithThumbnails(dbs: List[DB]) = {
+    dbs.foldLeft(Map[String, Taxon]())((db_agg, db) => {
+      val stocks = toStream(db.tables.stocks)
 
-  val taxonMapWithThumbnails = dbs.foldLeft(Map[String, Taxon]())((db_agg, db) => {
-    val stocks = toStream(db.tables.stocks)
+      val stocksHeader = stocks.next()
+      val specCodeLabel = "SpecCode"
+      val famCodeLabel = "FamCode"
+      val genCodeLabel = "GenCode"
 
-    val stocksHeader = stocks.next()
-    val specCodeLabel = "SpecCode"
-    val famCodeLabel = "FamCode"
-    val genCodeLabel = "GenCode"
-
-    val specCodes = stocks.flatMap(line => stocksHeader.zip(line).toMap.filterKeys(specCodeLabel == _).values)
-    val specCodesDistinct = specCodes.toSeq.distinct.toSet
+      val specCodes = stocks.flatMap(line => stocksHeader.zip(line).toMap.filterKeys(specCodeLabel == _).values)
+      val specCodesDistinct = specCodes.toSeq.distinct.toSet
 
 
-    val taxa = toStream(db.tables.taxa)
-    val taxaHeader = taxa.next()
-    val taxonMap = taxa.foldLeft(db_agg)((agg, line) => {
-      val taxonLine = taxaHeader.zip(line).toMap
-      taxonLine.get(specCodeLabel) match {
-        case Some(specCode) => if (specCodesDistinct.contains(specCode)) {
-          val rankNames = List("Class", "Order", "Family", "SubFamily", "Genus", "Species")
-          val aTaxon = Taxon(id = db.toExternalId(specCodeLabel, specCode),
-            name = List(taxonLine("Genus"), taxonLine("Species")).mkString(" "),
-            rank = "Species",
-            pathNames = rankNames,
-            pathIds = List(None, None, Some(famCodeLabel), None, Some(genCodeLabel), Some(specCodeLabel)).map {
-              case Some(pathId) => Some(db.toExternalId(pathId, taxonLine.getOrElse(pathId, "")))
-              case None => None
-            },
-            path = rankNames.map(rankName => {
-              taxonLine.get(rankName) match {
-                case Some(taxonName) => {
-                  if (taxonName == "null") {
-                    None
-                  } else {
-                    Some(taxonName)
+      val taxa = toStream(db.tables.taxa)
+      val taxaHeader = taxa.next()
+      val taxonMap = taxa.foldLeft(db_agg)((agg, line) => {
+        val taxonLine = taxaHeader.zip(line).toMap
+        taxonLine.get(specCodeLabel) match {
+          case Some(specCode) => if (specCodesDistinct.contains(specCode)) {
+            val rankNames = List("Class", "Order", "Family", "SubFamily", "Genus", "Species")
+            val aTaxon = Taxon(id = db.toExternalId(specCodeLabel, specCode),
+              name = List(taxonLine("Genus"), taxonLine("Species")).mkString(" "),
+              rank = "Species",
+              pathNames = rankNames,
+              pathIds = List(None, None, Some(famCodeLabel), None, Some(genCodeLabel), Some(specCodeLabel)).map {
+                case Some(pathId) => Some(db.toExternalId(pathId, taxonLine.getOrElse(pathId, "")))
+                case None => None
+              },
+              path = rankNames.map(rankName => {
+                taxonLine.get(rankName) match {
+                  case Some(taxonName) => {
+                    if (taxonName == "null") {
+                      None
+                    } else {
+                      Some(taxonName)
+                    }
                   }
+                  case _ => None
                 }
-                case _ => None
-              }
-            }),
-            externalUrl = db.idToUrl(specCode))
-          agg ++ Map(specCode -> aTaxon)
-        } else {
-          agg
-        }
-        case None => agg
-      }
-    })
-
-    val species = toStream(db.tables.species)
-    val speciesHeader = species.next()
-    species.foldLeft(taxonMap)((agg, line) => {
-      val speciesLine = speciesHeader.zip(line).toMap
-      (speciesLine.get(specCodeLabel), speciesLine.get("PicPreferredName")) match {
-        case (Some(specCode), Some(picName)) => {
-          agg.get(specCode) match {
-            case Some(taxon) => {
-              val thumbnailUrl = db.imageNameToUrl(picName)
-              agg ++ Map(specCode -> taxon.withThumbnail(thumbnailUrl))
-            }
-            case None => agg
+              }),
+              externalUrl = db.idToUrl(specCode))
+            agg ++ Map(specCode -> aTaxon)
+          } else {
+            agg
           }
+          case None => agg
         }
-        case _ => agg
-      }
+      })
+
+      val species = toStream(db.tables.species)
+      val speciesHeader = species.next()
+      species.foldLeft(taxonMap)((agg, line) => {
+        val speciesLine = speciesHeader.zip(line).toMap
+        (speciesLine.get(specCodeLabel), speciesLine.get("PicPreferredName")) match {
+          case (Some(specCode), Some(picName)) => {
+            agg.get(specCode) match {
+              case Some(taxon) => {
+                val thumbnailUrl = db.imageNameToUrl(picName)
+                agg ++ Map(specCode -> taxon.withThumbnail(thumbnailUrl))
+              }
+              case None => agg
+            }
+          }
+          case _ => agg
+        }
+      })
     })
-  })
+  }
 
   val taxonCacheHeader: List[String] = List("id", "name", "rank", "commonNames", "path", "pathIds", "pathNames", "externalUrl", "thumbnailUrl")
   println(taxonCacheHeader.mkString("\t"))
 
-  taxonMapWithThumbnails.values.map { taxon =>
+  private val slbValues = taxonMapWithThumbnails(List(SeaLifeBase)).values
+  private val fbValues = taxonMapWithThumbnails(List(FishBase)).values
+  (slbValues ++ fbValues).map { taxon =>
     List(taxon.id, taxon.name, taxon.rank, taxon.commonNames.map(_.getOrElse("")).mkString("|"),
       taxon.path.map(_.getOrElse("")).mkString("|"), taxon.pathIds.map(_.getOrElse("")).mkString("|"),
       taxon.pathNames.mkString("|"),
